8dfbb83dd49133c967007fa4cc6debda37320dae
kellysoh <so37@students.calvin.edu>
2019-02-09 11:57:39 -0500

    MQTT connection is successful!

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-const http = require('http');
-const static = require('node-static');
-const file = new static.Server('./');
-
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var app = express();
 var mqtt = require('mqtt')
+
 var options = {
-    port: 3000, 
-    host: 'mqtt://m11.cloudmqtt.com',
+    port: 17657, 
+    host: 'mqtt://m16.cloudmqtt.com',
     clientId: 'mqttjs_' + Math.random().toString(16).substr(2,8),
     username: 'hgyfcdvp',
     password: '7xO7C0uC6s-U',
@@ -16,6 +17,21 @@ var options = {
     clean: true,
     encoding: 'utf8'
 };
+
+var client = mqtt.connect('mqtt://m16.cloudmqtt.com', options);
+client.on('connect', function() { 
+    console.log('mqtt connected');
+    client.subscribe('Calvin/Parkinglot14/row1/spot1', function() {
+        client.on('message', function(topic, message, packet) {
+            console.log('Received '+ message + " on " + topic + " ");
+        });
+    });
+})
+const http = require('http');
+const static = require('node-static');
+const file = new static.Server('./');
+
+
 
 const server = http.createServer((req,res) => {
     req.addListener('end', () => file.serve(req, res)).resume();
